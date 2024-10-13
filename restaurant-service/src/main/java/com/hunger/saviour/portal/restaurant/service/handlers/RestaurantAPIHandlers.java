dbada03246bbd31ee61fd643fb7f2001ae377ab1
unikdahal <unikdahal03@gmail.com>
2024-10-13 16:56:47 +0530

    Created a GlobalExceptionHandler in Common

--- restaurant-service/src/main/java/com/hunger/saviour/portal/restaurant/service/handlers/RestaurantAPIHandlers.java
+++ restaurant-service/src/main/java/com/hunger/saviour/portal/restaurant/service/handlers/RestaurantAPIHandlers.java
@@ -1,33 +1,7 @@
 package com.hunger.saviour.portal.restaurant.service.handlers;
 
-import com.hunger.saviour.portal.restaurant.service.dtos.ResponseDTO;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.hungersaviour.portal.commons.handlers.HungerSaviourGlobalExceptionHandler;
 
-import java.time.LocalDateTime;
+public class RestaurantAPIHandlers extends HungerSaviourGlobalExceptionHandler {
 
-@RestControllerAdvice
-@Slf4j
-public class RestaurantAPIHandlers {
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseDTO> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        log.info("Entered into RestaurantAPIHandlers class with exception MethodArgumentNotValidException");
-        return new ResponseEntity<>(ResponseDTO.builder()
-                .errorMessages(
-                        exception.getBindingResult()
-                                .getAllErrors()
-                                .stream()
-                                .map(ObjectError::getDefaultMessage)
-                                .toList()
-                )
-                .statusCodeDescription(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .timestamp(LocalDateTime.now())
-                .build(), HttpStatus.BAD_REQUEST);
-    }
 }
---
+++ user-service/src/main/java/com/hungersaviour/user/service/handlers/UserServiceAPIHandler.java
@@ -0,0 +1,6 @@
+package com.hungersaviour.user.service.handlers;
+
+import com.hungersaviour.portal.commons.handlers.HungerSaviourGlobalExceptionHandler;
+
+public class UserServiceAPIHandler extends HungerSaviourGlobalExceptionHandler {
+}
