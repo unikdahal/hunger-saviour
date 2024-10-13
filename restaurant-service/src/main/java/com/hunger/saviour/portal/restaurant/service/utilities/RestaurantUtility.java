cf9dbbb5d29846b0720d2364b93a453959a89bc3
unikdahal <unikdahal03@gmail.com>
2024-10-13 19:15:49 +0530

    Added some utilities method for DTO to Entity and Entity to DTO Conversion

--- restaurant-service/src/main/java/com/hunger/saviour/portal/restaurant/service/utilities/RestaurantUtility.java
+++ restaurant-service/src/main/java/com/hunger/saviour/portal/restaurant/service/utilities/RestaurantUtility.java
@@ -1,29 +1,60 @@
 package com.hunger.saviour.portal.restaurant.service.utilities;
 
 import com.hunger.saviour.portal.restaurant.service.entities.RestaurantEntity;
+import com.hunger.saviour.portal.restaurant.service.entities.RestaurantMenuEntity;
 import com.hungersaviour.portal.commons.dtos.RestaurantDTO;
+import com.hungersaviour.portal.commons.dtos.RestaurantMenuDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RestaurantUtility {
 
     public static RestaurantEntity convertDTOToEntity(RestaurantDTO restaurantDTO) {
+        List<RestaurantMenuEntity> restaurantMenuEntities = restaurantDTO.getMenuItems().stream()
+                .map(RestaurantUtility::convertDTOToEntity)
+                .collect(Collectors.toList());
+
         return RestaurantEntity.builder()
                 .restaurantName(restaurantDTO.getRestaurantName())
                 .rating(restaurantDTO.getRating())
                 .location(restaurantDTO.getLocation())
                 .imageUrl(restaurantDTO.getImageUrl())
                 .menuTypes(restaurantDTO.getMenuTypes())
-                .menuItems(restaurantDTO.getMenuItems())
+                .menuItems(restaurantMenuEntities)
+                .build();
+    }
+
+    public static RestaurantMenuEntity convertDTOToEntity(RestaurantMenuDTO restaurantMenuDTO) {
+        return RestaurantMenuEntity.builder()
+                .menuImageUrl(restaurantMenuDTO.getMenuImageUrl())
+                .description(restaurantMenuDTO.getDescription())
+                .menuType(restaurantMenuDTO.getMenuType())
+                .menuItemPrice(restaurantMenuDTO.getMenuItemPrice())
                 .build();
     }
 
     public static RestaurantDTO convertEntityToDTO(RestaurantEntity restaurantEntity) {
+        List<RestaurantMenuDTO> restaurantMenuDTOList = restaurantEntity.getMenuItems().stream()
+                .map(RestaurantUtility::convertEntityToDTO)
+                .collect(Collectors.toList());
+
         return RestaurantDTO.builder()
                 .restaurantName(restaurantEntity.getRestaurantName())
                 .rating(restaurantEntity.getRating())
                 .location(restaurantEntity.getLocation())
                 .imageUrl(restaurantEntity.getImageUrl())
                 .menuTypes(restaurantEntity.getMenuTypes())
-                .menuItems(restaurantEntity.getMenuItems())
+                .menuItems(restaurantMenuDTOList)
+                .build();
+    }
+
+    public static RestaurantMenuDTO convertEntityToDTO(RestaurantMenuEntity restaurantMenuEntity) {
+        return RestaurantMenuDTO.builder()
+                .menuImageUrl(restaurantMenuEntity.getMenuImageUrl())
+                .description(restaurantMenuEntity.getDescription())
+                .menuType(restaurantMenuEntity.getMenuType())
+                .menuItemPrice(restaurantMenuEntity.getMenuItemPrice())
                 .build();
     }
 }
