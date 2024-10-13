4c0ed15e060772f7210125ad8007a809a80cc01f
unikdahal <unikdahal03@gmail.com>
2024-10-13 19:14:12 +0530

    Added Enable Discovery Client Annotation

--- restaurant-service/src/main/java/com/hunger/saviour/portal/restaurant/service/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/hunger/saviour/portal/restaurant/service/RestaurantServiceApplication.java
@@ -2,8 +2,10 @@ package com.hunger.saviour.portal.restaurant.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RestaurantServiceApplication {
 
     public static void main(String[] args) {
